2f7a7df482bce1286f49ed4feede91a744b2db86
Christine-Muchiri <christinemuchiri749@gmail.com>
2025-06-05 16:18:30 +0300

    fastapi integration

--- whistle-backend/hello_world/app.py
+++ whistle-backend/hello_world/app.py
@@ -1,42 +1,27 @@
-import json
+from fastapi import FastAPI, File, UploadFile, Form
+from fastapi.responses import JSONResponse
+from typing import List, Optional
+import uuidi
+import boto3
+import os
 
-# import requests
+app = FastAPI()
 
+@app.get("/")
+def root():
+    return {"message": "Whistle backend running"}
 
-def lambda_handler(event, context):
-    """Sample pure Lambda function
-
-    Parameters
-    ----------
-    event: dict, required
-        API Gateway Lambda Proxy Input Format
-
-        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
-
-    context: object, required
-        Lambda Context runtime methods and attributes
-
-        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
-
-    Returns
-    ------
-    API Gateway Lambda Proxy Output Format: dict
-
-        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
-    """
-
-    # try:
-    #     ip = requests.get("http://checkip.amazonaws.com/")
-    # except requests.RequestException as e:
-    #     # Send some context about this error to Lambda Logs
-    #     print(e)
-
-    #     raise e
-
-    return {
-        "statusCode": 200,
-        "body": json.dumps({
-            "message": "hello world",
-            # "location": ip.text.replace("\n", "")
-        }),
-    }
+@app.post("/leaks")
+async def drop_leak(
+    text: str = Form(...),
+    hashtags: Optional[str] = Form(""),
+    image: Optional[UploadFile] = File(None),
+    video: Optional[UploadFile] = File(None)
+):
+    leak_id = str(uuid.uuid4())
+    # TODO: upload media to s3 and save metadata to DynamoDB
+    
+    return JSONResponse({
+        "leak_id": leak_id,
+        "status": "saved(mock)",
+    })
