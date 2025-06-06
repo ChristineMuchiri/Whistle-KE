a7f6e8c1e006feefd62e8aead1447d694b6cea8f
Christine-Muchiri <christinemuchiri749@gmail.com>
2025-06-06 20:26:18 +0300

    create-alias

--- whistle-backend/venv/app.py
+++ whistle-backend/venv/app.py
@@ -1,7 +1,28 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Form
+import bcrypt
 
 app = FastAPI()
 
+# simulated user DB
+user_db = {}
+
 @app.get("/")
 async def root():
-    return {"message": "whistle backend running"}
+    return {"message": "whistle backend running. Create Alias # Alias"}
+
+@app.post("/create-alias")
+async def create_alias(alias: str = Form(...), password: str = Form(...)):
+    if alias in user_db:
+        raise HTTPException(status_code=400, detail="Alias already exists. Pick another one")
+    
+    # hash the password
+    salt = bcrypt.gensalt()
+    hashed_pw = bcrypt.hashpw(password.encode(), salt)
+    
+    # store user with hashed password
+    user_db[alias] = {
+        "alias": alias,
+        "password_hash": hashed_pw.decode() # to store as str
+    }
+    
+    return {"message": "Alias created successfully"}
