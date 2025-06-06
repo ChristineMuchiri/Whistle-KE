a6d17af02b71c07dee53d4aa355c003ec54f363b
Christine-Muchiri <christinemuchiri749@gmail.com>
2025-06-06 21:13:51 +0300

    create alias fastapi route

--- whistle-backend/venv/app.py
+++ whistle-backend/venv/app.py
@@ -1,10 +1,17 @@
 from fastapi import FastAPI, HTTPException, Form
 import bcrypt
+import boto3
+import os
 
 app = FastAPI()
 
-# simulated user DB
-user_db = {}
+# user DB
+dynamodb = boto3.resource(
+    'dynamodb',
+    region_name='us-west-2',
+    endpoint_url='http://localhost:8000'
+)
+users_table = dynamodb.Table("Users")
 
 @app.get("/")
 async def root():
@@ -12,17 +19,22 @@ async def root():
 
 @app.post("/create-alias")
 async def create_alias(alias: str = Form(...), password: str = Form(...)):
-    if alias in user_db:
+    #check if user exists
+    response = users_table.get_item(Key={'alias': alias})
+    if 'Item' in response:
         raise HTTPException(status_code=400, detail="Alias already exists. Pick another one")
     
     # hash the password
     salt = bcrypt.gensalt()
-    hashed_pw = bcrypt.hashpw(password.encode(), salt)
+    hashed_pw = bcrypt.hashpw(password.encode(), salt).decode() # to store as str
     
     # store user with hashed password
-    user_db[alias] = {
-        "alias": alias,
-        "password_hash": hashed_pw.decode() # to store as str
-    }
+    users_table.put_item(
+        Item={
+           "alias": alias,
+        "password_hash": hashed_pw 
+        }
+    )
+        
     
     return {"message": "Alias created successfully"}
