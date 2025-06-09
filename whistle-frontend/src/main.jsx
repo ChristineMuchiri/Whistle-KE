5fd228f6e8138e65a75c5d9150196683d5faa60e
Christine-Muchiri <christinemuchiri749@gmail.com>
2025-06-09 10:25:04 +0300

    adding aws amplify

--- whistle-frontend/src/main.jsx
+++ whistle-frontend/src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { Amplify } from 'aws-amplify';
+import awsExports from './aws-exports';
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+Amplify.configure(awsExports);
+
+const root = createRoot(document.getElementById('root'));
+root.render(<App />);
+
